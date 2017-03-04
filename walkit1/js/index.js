264bc26d5bdacceb13e63aef73f4e269f674dc39
larryschirmer <larryschirmer@gmail.com>
2017-03-04 18:52:18 -0500

    make a single pin show

--- walkit1/js/index.js
+++ walkit1/js/index.js
@@ -1,16 +1,28 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibGFycnlzY2hpcm1lciIsImEiOiJjaXp2cnRtZzMwMWRzMzNtbXIwNzhoM3hvIn0.naKjJ_zeLB74jpMKbIBLSQ';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-    center: [-83.040639, 42.346611], // starting position
-    zoom: 13 // starting zoom
-});
+L.mapbox.accessToken = 'pk.eyJ1IjoibGFycnlzY2hpcm1lciIsImEiOiJjaXp2cnRtZzMwMWRzMzNtbXIwNzhoM3hvIn0.naKjJ_zeLB74jpMKbIBLSQ';
 
-var el = document.createElement('div');
-el.className = 'marker';
-el.style.width = '10px';
-el.style.height = '10px';
+var map = L.mapbox.map('map', 'mapbox.streets').setView([42.346611,-83.040639], 16);
 
-var marker = new mapboxgl.Marker(el)
-  .setLngLat([-83.040639, 42.346611])
-  .addTo(map);
+
+    L.mapbox.featureLayer({
+        // this feature is in the GeoJSON format: see geojson.org
+        // for the full specification
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            // coordinates here are in longitude, latitude order because
+            // x, y is the standard for GeoJSON and many formats
+            coordinates: [
+              -83.040639,
+              42.346611
+            ]
+        },
+        properties: {
+            title: 'Peregrine Espresso',
+            description: '1718 14th St NW, Washington, DC',
+            // one can customize markers by adding simplestyle properties
+            // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+            'marker-size': 'large',
+            'marker-color': '#BE9A6B',
+            'marker-symbol': 'cafe'
+        }
+    }).addTo(map);
